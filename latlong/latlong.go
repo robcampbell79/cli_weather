e7cc44b22303f63681fe0b24dc5a4ad8c1828202
robert <robcampbell@ucmo.edu>
2023-02-16 15:51:36 -0600

    Check for empty struct

--- latlong/latlong.go
+++ latlong/latlong.go
@@ -16,7 +16,7 @@ type LongLat struct {
 	Lng string `json:"longitude"`
 }
 
-func GetLatLong(state string, city string) (string) {
+func GetLatLong(state string, city string) string {
 	zippo := "http://api.zippopotam.us/us/"+state+"/"+city
 
 	zr, err := http.Get(zippo)
@@ -44,4 +44,34 @@ func GetLatLong(state string, city string) (string) {
 	latlng := lat+","+lng
 
 	return latlng
+}
+
+func CheckIfEmpty(state string, city string) int {
+	var isEmpty int
+
+	zippo := "http://api.zippopotam.us/us/"+state+"/"+city
+
+	zr, err := http.Get(zippo)
+	if err != nil {
+		return 1
+	} 
+
+	defer zr.Body.Close()
+
+	zBody, err := ioutil.ReadAll(zr.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var pl Places
+
+	json.Unmarshal(zBody, &pl)
+
+	if (len(pl.Place) < 1) {
+		isEmpty = 1
+	} else {
+		isEmpty = 0
+	}
+
+	return isEmpty
 }
